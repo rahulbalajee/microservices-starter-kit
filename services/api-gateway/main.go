c6569b8908893ab5e35801ebc8f1616dbc6f72c9
rahulbalajee <rahrkb4@gmail.com>
2026-02-08 09:26:57 +0530

    terminate requests gracefully

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"ride-sharing/shared/env"
 )
@@ -19,11 +24,38 @@ func main() {
 	mux.HandleFunc("POST /trip/preview", handleTripPreview)
 
 	srv := &http.Server{
-		Addr:    httpAddr,
-		Handler: mux,
+		Addr:              httpAddr,
+		Handler:           mux,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+		IdleTimeout:       30 * time.Second,
 	}
 
-	if err := srv.ListenAndServe(); err != nil {
-		log.Fatalf("HTTP server error: %v\n", err)
+	serverErr := make(chan error, 1)
+
+	go func() {
+		log.Printf("server listening on %s\n", httpAddr)
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			serverErr <- err
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErr:
+		log.Printf("error starting the server: %v\n", err)
+	case sig := <-shutdown:
+		log.Printf("server is shutting down due to %v signal", sig)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("could not shutdown server gracefully %v", err)
+			srv.Close()
+		}
 	}
 }
