a4a0cf52610877a707f0e0cc7f0cd2648a331ec5
rahulbalajee <rahrkb4@gmail.com>
2026-02-08 07:35:41 +0530

    ai gateway

--- services/api-gateway/handlers.go
+++ services/api-gateway/handlers.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
-	"io"
 	"log"
-	"maps"
 	"net/http"
 	"time"
 
+	"ride-sharing/shared/contracts"
 	"ride-sharing/shared/env"
 )
 
@@ -25,9 +25,16 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	jsonBody, err := json.Marshal(reqBody)
+	if err != nil {
+		http.Error(w, "failed to parse JSON data", http.StatusBadRequest)
+		return
+	}
+	reader := bytes.NewReader(jsonBody)
+
 	// Call trip service (use K8s service name in cluster, localhost for local dev)
 	tripServiceURL := env.GetString("TRIP_SERVICE_URL", "http://localhost:8083")
-	req, err := http.NewRequest(http.MethodGet, tripServiceURL+"/preview", nil)
+	req, err := http.NewRequest(http.MethodPost, tripServiceURL+"/preview", reader)
 	if err != nil {
 		http.Error(w, "error framing request", http.StatusInternalServerError)
 		return
@@ -45,11 +52,12 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 	}
 	defer resp.Body.Close()
 
-	// Forward trip service response to client (status, headers, body)
-	maps.Copy(w.Header(), resp.Header)
-	w.WriteHeader(resp.StatusCode)
+	var respBody any
+	if err = json.NewDecoder(resp.Body).Decode(&respBody); err != nil {
+		http.Error(w, "failed to parse JSON data", http.StatusBadRequest)
+		return
+	}
 
-	io.Copy(w, resp.Body)
-
-	//writeJSON(w, resp.StatusCode, nil)
+	response := contracts.APIResponse{Data: respBody}
+	writeJSON(w, http.StatusCreated, response)
 }
