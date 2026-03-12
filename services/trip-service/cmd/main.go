4c3450a715b35350eb08e4320cda8e82be91fc17
rahulbalajee <rahrkb4@gmail.com>
2026-03-12 08:52:07 +0530

    mongo configuration

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"ride-sharing/services/trip-service/internal/infrastructure/grpc"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
+	"ride-sharing/shared/db"
 	"ride-sharing/shared/env"
 	"ride-sharing/shared/messaging"
 	"ride-sharing/shared/tracing"
@@ -50,6 +51,15 @@ func main() {
 
 	inmemRepo := repository.NewInmemRepository()
 	svc := service.NewService(inmemRepo)
+
+	mongoClient, err := db.NewMongoClient(ctx, db.NewMongoDefaultConfig())
+	if err != nil {
+		log.Fatalf("failed to init MongoDB, err: %v", err)
+	}
+	defer mongoClient.Disconnect(ctx)
+
+	mongoDb := db.GetDatabase(mongoClient, db.NewMongoDefaultConfig())
+	log.Println(mongoDb.Name())
 
 	log.Println("starting rabbitMQ connection")
 	mq, err := messaging.NewRabbitMQ(rabbitmqURI)
